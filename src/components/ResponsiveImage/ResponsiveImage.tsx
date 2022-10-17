8c722df0cb3dc9a458737e4fec69940df0e24260
Tilbert Balaban <tilbert.balaban@gmail.com>
2022-10-17 11:35:28 +0300

    feat: add responsive images #feature/seo_and_content_improvements

--- src/components/ResponsiveImage/ResponsiveImage.tsx
+++ src/components/ResponsiveImage/ResponsiveImage.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react'
 
 const ResponsiveImage = ({ src, mobileSrc, className, ...rest }: any) => (
-  <>
-    {console.log('qqq', className)}
-    <img
-      src={mobileSrc || src}
-      srcSet={`${mobileSrc || src}, ${src}`}
-      loading="lazy"
-      className={className}
-      {...rest}
-    />
-  </>
+  <img
+    src={mobileSrc || src}
+    srcSet={`${mobileSrc || src}, ${src}`}
+    loading="lazy"
+    className={className}
+    {...rest}
+  />
 )
 
 export default ResponsiveImage
